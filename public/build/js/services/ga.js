02d4af6671ee796dab5d0d6d34925db2fae1e0ea
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-02-03 13:07:27 -0200

    Build

--- public/build/js/services/ga.js
+++
@@ -1,6 +0,0 @@
-angular.module('GoogleAnalytics', [])
-.run(function ($rootScope, $location) {
-  $rootScope.$on("$routeChangeSuccess",function(event, next, current){
-    ga('send', 'pageview', { page: $location.path(), title: $rootScope.title });
-  });
-});
